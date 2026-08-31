52fb64abce1175a8d6e3b678eae791fbcb7a0ab4
charlescezarmendes215-lab <charlescezarmendes215@gmail.com>
2026-08-30 23:07:38 -0300

    Change HTML language attribute from 'en' to 'pt-BR'

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -113,7 +113,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
 
 function RootShell({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <head>
         <HeadContent />
       </head>
@@ -130,7 +130,6 @@ function RootComponent() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      {/* Required: nested routes render here. Removing <Outlet /> breaks all child routes. */}
       <Outlet />
     </QueryClientProvider>
   );
